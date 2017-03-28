2e9799d55bdcab81dcadc2883b151deb4576368d
Austin Green <austinzgreen@gmail.com>
2017-03-27 22:15:29 -0400

    tutorial

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
+import Photo from './Photo';
 
-const PhotoGrid = React.createClass({
+class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        Grid
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
       </div>
     );
-  },
-});
+  }
+}
 
 export default PhotoGrid;
